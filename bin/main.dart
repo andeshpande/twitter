51348fbe86fcde22934fbac1df5e51f0f00134ce
Aniruddha Deshpande <a.deshpande@student.tudelft.nl>
2015-03-21 00:45:29 +0100

    Twitter authorization automated(crudely)

--- bin/main.dart
+++ bin/main.dart
@@ -1,39 +1,65 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:twitter/twitter.dart' as twitter;
-import 'dart:async';
 import 'dart:convert';
-import 'package:oauth/oauth.dart' as oauth;
 import 'dart:io';
-import 'package:json_object/json_object.dart';
+import 'dart:math' as math;
+import 'package:crypto/crypto.dart' as crypto;
+import 'dart:core';
 
-class TwitterKey {
-
-  oauth.Token consumer;
-  oauth.Token user;
-
-  TwitterKey.createKey(String consumerKey,String consumerSecret,
-                              String accessToken,String accessSecret) {
-
-    this.consumer = createToken(consumerKey,consumerSecret);
-    this.user = createToken(accessToken,accessSecret);
-  }
-
+String create_nonce(){
+  math.Random rnd = new math.Random();
+  List<int> values = new List<int>.generate(32, (i) => rnd.nextInt(256));
+  var oauth_nonce = crypto.CryptoUtils.bytesToBase64(values).replaceAll(new RegExp('[=/+]'), '');
+  return oauth_nonce;
+}
+String computeSignature(List<int> key, List<int> signatureBase) {
+  var mac = new crypto.HMAC(new crypto.SHA1(), key);
+  mac.add(signatureBase);
+  return crypto.CryptoUtils.bytesToBase64(mac.close());
 }
 
-oauth.Token createToken(String key, String secret) {
-  return new oauth.Token(key, secret);
+String getAuthorizationHeaders() {
+  String oauth_consumer_key     = 'QSW4maKbx5jsWzDLxg48DPsO6';                            // Get Consumer Key from http://apps.twitter.com
+    String oauth_consumer_secret  = 'xCLaSQcJPc3Mj4ATSFpsBUhCSlA81P2QPD7Ayc3c411x7sUrte'; // Get Consumer Secret from http://apps.twitter.com
+    String oauth_token            = '195336429-GNz0lzQybqlLpX6xLKjeAO8H33aI2738ZAo6C79e'; // Get Access Key from http://apps.twitter.com
+    String oauth_access_secret    = '2E4oUKx0ftBilcnrPif4b6UM8JgePJjzQ17g86qvdNrQE';      // Get Access Secret from http://apps.twitter.com
+
+    String oauth_method           = 'HMAC-SHA1';
+    String oauth_version          = '1.0';
+    String oauth_nonce            = create_nonce();
+    String timestamp              = (new DateTime.now().millisecondsSinceEpoch ~/ 1000).toString();
+   
+    // A very crude way to create base string for oauth
+    String base_string            = "GET&https%3A%2F%2Fstream.twitter.com%2F1.1%2Fstatuses%2Fsample.json&"
+                                    +"oauth_consumer_key%3D"        + oauth_consumer_key
+                                    +"%26oauth_nonce%3D"            + oauth_nonce
+                                    +"%26oauth_signature_method%3D" + oauth_method
+                                    +"%26oauth_timestamp%3D"        + timestamp
+                                    +"%26oauth_token%3D"            + oauth_token 
+                                    +"%26oauth_version%3D"          + oauth_version;
+   
+          
+    String key                    = Uri.encodeComponent(oauth_consumer_secret) + '&' + Uri.encodeComponent(oauth_access_secret);
+    String oauth_signature        = Uri.encodeComponent(computeSignature(UTF8.encode(key), UTF8.encode(base_string)));
+     
+    var authorization = 'OAuth oauth_consumer_key="' + oauth_consumer_key 
+        +'", oauth_nonce="'                          + oauth_nonce
+        +'", oauth_signature="'                      + oauth_signature
+        +'", oauth_signature_method="'               + oauth_method
+        +'", oauth_timestamp="'                      + timestamp
+        +'", oauth_token="'                          + oauth_token
+        +'", oauth_version="'                        + oauth_version
+        +'"';
+    
+    return authorization;
 }
-
-
 
 main() {
  
-  // authorization string from https://dev.twitter.com/oauth/tools/signature-generator/7220915?nid=875
-  var authorization = 'OAuth oauth_consumer_key="PLLYguQdJqooNfAKwTlZe5MMi", oauth_nonce="b3b3626a4c5806bcc8601d98fe7d0d51", oauth_signature="lare0FLb8SNyITNJlJYkXLLEKH8%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1426774910", oauth_token="2238912169-ThdEALf8Wxbg1aCpIVEMWD64tphM1hXipLJe2MM", oauth_version="1.0"';
+  var authorization = getAuthorizationHeaders();
+  // print(authorization);
   var url = "https://stream.twitter.com/1.1/statuses/sample.json";
-  
   new HttpClient().getUrl(Uri.parse(url))
   
   .then((HttpClientRequest request) {
